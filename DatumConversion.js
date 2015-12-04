5e3dd17253bc5050716e85b95f4d05aec405c387
Geoff Chapman <geoff.chapman@mac.com>
2015-12-04 18:15:33 +0000

    Initial commit of Datum Conversion class
    creation of a object of the conversion of coordinates from one datum to
    another using helmet transformations

---
+++ DatumConversion.js
@@ -0,0 +1,23 @@
+DatumConversion = function () {};
+
+// Utility function to convert degrees to radians
+Math.deg2Rad = function(degrees) {
+	return degrees * Math.PI / 180;
+};
+
+// Utility function to convert radians to degrees
+Math.rad2Deg = function(radians) {
+	return radians * 180 / Math.PI;
+};
+
+
+DatumConversion.toCartesian = function (coordinates, ellipsoidCode) {
+
+
+
+};
+
+DatumConversion.fromCartesian = function (coordinates, ellipsoidCode) {
+
+
+}
