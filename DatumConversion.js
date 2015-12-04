c280cec62c0539213e1fedbc65f016143a6e2ce1
Geoff Chapman <geoff.chapman@mac.com>
2015-12-04 18:18:51 +0000

    LatLon to Cartesian method added
    Addition of method for converting LatLon coordinates to Cartesian
    coordinates, required for Helmert transformation

--- DatumConversion.js
+++ DatumConversion.js
@@ -13,7 +13,28 @@ Math.rad2Deg = function(radians) {
 
 DatumConversion.toCartesian = function (coordinates, ellipsoidCode) {
 
+	var ellipoid = ellipoidConstants[0][ellipsoidCode];
+	
+	var lon = Math.deg2Rad(coordinates.getXAxis());
+	var lat = Math.deg2Rad(coordinates.getYAxis());
+	var height = coordinates.getZAxis();
 
+	var semiMajor = ellipoid.a
+	var semiMinor = ellipoid.b
+	var e2 = TransversMercatorConversion.eccentricitySquared(semiMajor,semiMinor);
+
+	var sinLat = Math.sin(lat);
+	var cosLat = Math.cos(lat);
+	var sinLon = Math.sin(lon);
+	var cosLon = Math.cos(lon);
+
+	var v = semiMajor / (Math.sqrt(1 - (e2 * Math.pow(sinLat,2))));
+	var x = (v + height) * cosLat * cosLon;
+	var y = (v + height) * cosLat * sinLon;
+	var z = ((1 - e2) * v + height) * sinLat;
+
+
+	return new xYValues(x,y,z);
 
 };
 
