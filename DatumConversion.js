0c5c9512d5873f4553eefe1c1e388f5348fb9cc3
Geoff Chapman <geoff.chapman@mac.com>
2015-12-04 18:22:05 +0000

    Cartesian to LatLon method added
    Addition of method for converting Cartesian coordinates to LatLon
    coordinates. Used after a Helmert transformation to plot new values

--- DatumConversion.js
+++ DatumConversion.js
@@ -40,5 +40,32 @@ DatumConversion.toCartesian = function (coordinates, ellipsoidCode) {
 
 DatumConversion.fromCartesian = function (coordinates, ellipsoidCode) {
 
+	console.log(coordinates.getXAxis());
+	var ellipoid = ellipoidConstants[0][ellipsoidCode];
+	
+	var x = coordinates.getXAxis();
+	var y = coordinates.getYAxis();
+	var z = coordinates.getZAxis();
 
+	var semiMajor = ellipoid.a
+	var semiMinor = ellipoid.b
+	var e2 = TransversMercatorConversion.eccentricitySquared(semiMajor,semiMinor);
+
+	var lon = Math.atan2(y , x);
+
+	var v = semiMajor / (Math.sqrt(1 - (e2 * Math.pow(Math.sin(y),2))));
+	var p = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+	var lat = Math.atan2(z , p * (1 - e2));
+
+	while (Math.abs(y - lat) > (4 / semiMajor)) {
+		y = lat;
+		var v = semiMajor / (Math.sqrt(1 - (e2 * Math.pow(Math.sin(y),2))));
+		var lat = Math.atan2((z + e2 * v * Math.sin(y)) , p);
+	}
+
+	var height = (p / Math.cos(lat)) - v;
+
+	lon = Math.rad2Deg(lon);
+	lat = Math.rad2Deg(lat);
+	return new lonLatValues(lon, lat, height);
 }
